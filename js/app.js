33f849634607dedd13538cbadd23a15bba6ca200
NishantSanghani1103 <nishantsanghani09@gmail.com>
2025-03-24 14:20:29 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,69 @@
+$('#dairyCarousal').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6.5
+        }
+    }
+})
+
+$('#tobacoCarosual').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6.5
+        }
+    }
+})
+$('#snacksCarosual').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6.5
+        }
+    }
+})
+$('#hookahCarousal').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6.5
+        }
+    }
+})
